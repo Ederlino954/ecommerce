2f48f2eb7f0ade921d36a1f5e46882ad32b26e8a
Ederlino de Almeida Camargos <ederlinoacamargos05@gmail.com>
2021-11-14 13:42:06 -0300

    Aula 111 Admin - Produtos - CRUD

---
+++ admin-products.php
@@ -0,0 +1,99 @@
+<?php
+
+use \Hcode\Model\User;
+use \Hcode\Model\Product;
+use \Hcode\PageAdmin;
+
+$app->get("/admin/products", function() {
+
+    user::verifyLogin();
+
+    $products = Product::listAll();
+
+    $page = new PageAdmin();
+
+    $page->setTpl("products", [
+        "products"=>$products
+    ]);	
+
+});
+
+$app->get("/admin/products/create", function() {
+
+    user::verifyLogin();    
+
+    $page = new PageAdmin();
+
+    $page->setTpl("products-create");	
+
+});
+
+$app->post("/admin/products/create", function() {
+
+    user::verifyLogin();    
+
+    $product = new Product();
+
+    $product->setData($_POST);    
+
+    $product->save(); // metodo dando erro - verificar posteriormente 
+
+    header("Location: /admin/products");
+    exit;
+
+});
+
+$app->get("/admin/products/:idproduct", function($idproduct) {
+
+    user::verifyLogin();    
+
+    $product = new Product();
+
+    $product->get((int)$idproduct);
+
+    $page = new PageAdmin();
+
+    $page->setTpl("products-update", [
+        'product'=>$product->getValues()
+    ]);	
+
+});
+
+$app->post("/admin/products/:idproduct", function($idproduct) {
+
+    user::verifyLogin();    
+
+    $product = new Product();
+
+    $product->get((int)$idproduct);
+
+    $product->setData($_POST);	
+
+    $product->save();
+
+    $product->setPhoto($_FILES['file']);
+
+    header("Location: /admin/products");
+    exit;
+
+});
+
+$app->get("/admin/products/:idproduct/delete", function($idproduct) {
+
+    user::verifyLogin();    
+
+    $product = new Product();
+
+    $product->get((int)$idproduct);
+
+    $product->delete();    
+
+    header("Location: /admin/products");
+    exit;
+
+});
+
+
+
+
+?>
