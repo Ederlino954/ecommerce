db42e3183907c04b8f33e9b7e9f3bebac9111d88
Ederlino de Almeida Camargos <ederlinoacamargos05@gmail.com>
2021-12-01 23:23:38 -0300

    Aula 116 - Site carrinho de compras- Layout e classes

--- site.php
+++ site.php
@@ -1,5 +1,6 @@
 <?php
 
+use Hcode\Model\Cart;
 use Hcode\Model\Category;
 use Hcode\Model\Product;
 use \Hcode\Page;
@@ -57,6 +58,16 @@ $app->get("/products/:desurl", function($desurl) {
 		'product'=>$product->getValues(),
 		'categories'=>$product->getCategories()
 	]);
+
+});
+
+$app->get("/cart", function() {	
+
+	$cart = Cart::getFromSession();
+
+	$page = new Page();
+
+	$page->setTpl("cart");
 
 });
 
