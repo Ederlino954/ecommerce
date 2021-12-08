77afa30655bc3d8da943ea0ccc5284d4269858de
Ederlino de Almeida Camargos <ederlinoacamargos05@gmail.com>
2021-12-08 00:45:16 -0300

    Aula 119 - Site Login

--- site.php
+++ site.php
@@ -1,8 +1,10 @@
 <?php
 
+use Hcode\Model\Address;
 use Hcode\Model\Cart;
 use Hcode\Model\Category;
 use Hcode\Model\Product;
+use Hcode\Model\User;
 use \Hcode\Page;
 
 $app->get("/", function() {
@@ -136,6 +138,63 @@ $app->post("/cart/freight", function() {
 	exit;
 
 });
+
+$app->get("/checkout" , function(){
+
+	User::verifyLogin(false);
+
+	$cart = Cart::getFromSession();
+
+	$address = new Address();
+
+	$page = new Page();
+
+	$page->setTpl("checkout", [
+		'cart'=>$cart->getValues(),
+		'address'=>$address->getValues()
+	]);
+
+});
+
+$app->get("/login" , function(){		
+
+	$page = new Page();
+
+	$page->setTpl("login", [		
+		'error'=>User::getError()
+	]
+);
+
+});
+
+$app->post("/login" , function(){		
+
+	try {
+		
+		User::login($_POST['login'], $_POST['password']);
+
+	} catch (\Exception $e) {
+		
+		User::setError($e->getMessage());
+
+	}	
+
+	header("Location: /checkout");
+	exit;
+
+});
+
+$app->get("/logout" , function(){		
+
+	User::logout();
+
+	header("Location: /login");
+	exit;
+
+});
+
+
+
 
 
 ?>
