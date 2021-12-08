157f46e2eb96fdd892f8d4b73b2d5363ba3d8737
Ederlino de Almeida Camargos <ederlinoacamargos05@gmail.com>
2021-12-08 13:46:13 -0300

    Aula 120 - Site - cadastro

--- site.php
+++ site.php
@@ -151,7 +151,7 @@ $app->get("/checkout" , function(){
 
 	$page->setTpl("checkout", [
 		'cart'=>$cart->getValues(),
-		'address'=>$address->getValues()
+		'address'=>$address->getValues(),
 	]);
 
 });
@@ -161,7 +161,10 @@ $app->get("/login" , function(){
 	$page = new Page();
 
 	$page->setTpl("login", [		
-		'error'=>User::getError()
+		'error'=>User::getError(),
+		'errorRegister'=>User::getErrorRegister(),
+		'registerValues'=>(isset($_SESSION['registerValues'])) ? $_SESSION['registerValues'] : ['name' => '','email' => '','phone' => ''],
+
 	]
 );
 
@@ -189,6 +192,63 @@ $app->get("/logout" , function(){
 	User::logout();
 
 	header("Location: /login");
+	exit;
+
+});
+
+$app->post("/register" , function(){
+	
+	$_SESSION['registerValues'] = $_POST;
+
+	if (!isset($_POST['name']) OR $_POST['name'] == '' ) {
+		
+		User::setErrorRegister("Preencha o seu nome.");
+		header("Location: /login");
+		exit;
+	}
+
+	$_SESSION['registerValues'] = $_POST;
+
+	if (!isset($_POST['email']) OR $_POST['email'] == '' ) {
+		
+		User::setErrorRegister("Preencha o seu e-mail.");
+		header("Location: /login");
+		exit;
+
+	}
+
+	if (!isset($_POST['password']) OR $_POST['password'] == '' ) {
+		
+		User::setErrorRegister("Preencha o sua senha.");
+		header("Location: /login");
+		exit;
+
+	}
+
+	if (User::checkLoginExist($_POST['email']) === true ) {
+		
+		User::setErrorRegister("Este email já está sendo usado!.");
+		header("Location: /login");
+		exit;
+
+	}
+
+	$user = new User();
+
+	$user->setData([
+		'inadmin'=>0,
+		'deslogin'=>$_POST['email'],
+		'desperson'=>$_POST['name'],
+		'desemail'=>$_POST['email'],
+		'despassword'=>$_POST['password'],
+		'nrphone'=>$_POST['phone'],
+	]);
+
+	$user->save();
+
+	User::login($_POST['email'], $_POST['password']);
+
+	header("Location: /checkout");
 	exit;
 
 });
